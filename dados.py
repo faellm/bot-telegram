50f5739cdc03dae52510e7f8e32364406fe9b380
Rafael Lara <92409857+faellm@users.noreply.github.com>
2022-11-23 13:51:47 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,7 +1,7 @@
-token = '5641112414:AAFEWgLvhz9k9ceyIrgb185hR7wbYm_VNQQ' #colocar o seu token da api, fornecida pelo BotFather
-chat_id1 = '5219044660' #colocar id do grupo 1
-chat_id2 = '5219044660' #colocar id do grupo 2 
+token = '' #colocar o seu token da api, fornecida pelo BotFather
+chat_id1 = '' #colocar id do grupo 1
+chat_id2 = '' #colocar id do grupo 2 
 
 #caso necessario mais grupos so seguir o parametro { variavel = 'id' }
 
-my_chat = '5219044660' # colocar id do seu chat no telegram, para receber a mensagem.
+my_chat = '' # colocar id do seu chat no telegram, para receber a mensagem.
